73a8fcf8322ef33634e36a4a56011b02e109fcd6
nulithic <admin@nulithic.dev>
2022-05-17 11:35:35 -0700

    navbar responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,29 +1,87 @@
-import { Box, AppBar, Toolbar, Typography, Container, Stack, Link } from "@mui/material";
+import { useState } from "react";
+import { Outlet, NavLink } from "react-router-dom";
+import {
+  Stack,
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  Container,
+  Button,
+  Drawer,
+  List,
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
 import navbar from "../../routing/navbar";
-import { Outlet } from "react-router-dom";
 
 export default function Navbar() {
+  const [anchorElNav, setAnchorElNav] = useState(false);
+
+  const toggleDrawer = () => {
+    setAnchorElNav((prev) => !prev);
+  };
+
   return (
     <Stack>
       <AppBar position="static">
-        <Container maxWidth={false}>
-          <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
-            <Typography variant="h4">Support Cartridge</Typography>
-            <Stack direction="row" spacing={2}>
+        <Container>
+          <Toolbar disableGutters>
+            <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              Support Cartridge
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, justifyContent: "flex-end", display: { xs: "flex", md: "none" } }}>
+              <IconButton size="large" onClick={toggleDrawer} color="inherit">
+                <MenuIcon />
+              </IconButton>
+
+              <Drawer anchor="top" open={anchorElNav} onClose={toggleDrawer}>
+                <Box sx={{ width: "auto" }} role="presentation" onClick={toggleDrawer} onKeyDown={toggleDrawer}>
+                  <List>
+                    {navbar.map((page, index) => (
+                      <Box key={page.id}>
+                        <ListItem>
+                          <ListItemButton component={NavLink} to={page.path}>
+                            <ListItemText primary={page.title} />
+                          </ListItemButton>
+                        </ListItem>
+                        {index + 1 !== navbar.length ? <Divider /> : null}
+                      </Box>
+                    ))}
+                  </List>
+                </Box>
+              </Drawer>
+            </Box>
+
+            <Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: "none", md: "flex" } }}>
+              Support Cartridge
+            </Typography>
+
+            <Stack direction="row" spacing={2} sx={{ flexGrow: 1, justifyContent: "flex-end", display: { xs: "none", md: "flex" } }}>
               {navbar.map((page) => (
-                <Link key={page.id} href={page.path}>
-                  <Typography variant="body" sx={{ color: "white" }}>
-                    {page.title}
-                  </Typography>
-                </Link>
+                <NavLink key={page.id} to={page.path} style={{ textDecoration: "none" }}>
+                  {({ isActive }) => (
+                    <Typography variant="body" sx={{ color: "white", textDecorationLine: isActive ? "underline" : "none" }}>
+                      {page.title}
+                    </Typography>
+                  )}
+                </NavLink>
               ))}
             </Stack>
           </Toolbar>
         </Container>
       </AppBar>
-      <Box sx={{ display: "flex", padding: "24px" }}>
+      <Container sx={{ display: "flex", padding: "24px" }}>
         <Outlet />
-      </Box>
+      </Container>
     </Stack>
   );
 }
